1a40e7afa2838de5687cecb15384b21cb3135b5e
Mark Olson <theothermarkolson@gmail.com>
2013-07-04 16:20:39 -0400

    Only use OLImage

--- Cat Streamer/SYNCatViewController.m
+++ Cat Streamer/SYNCatViewController.m
@@ -10,7 +10,6 @@
 #import "SYNCatHerder.h"
 #import "SYNRootViewController.h"
 #import "AFNetworking.h"
-#import "UIImage+animatedGIF.h"
 
 
 @interface SYNCatViewController ()
@@ -39,9 +38,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"nyan-cat-white-background" ofType:@"gif"];  
-    NSData *cat = [NSData dataWithContentsOfFile:filePath];
-    [imageView setImage:[UIImage animatedImageWithAnimatedGIFData:cat] ];
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"nyan-cat-white-background" ofType:@"gif"];
+    [imageView setImage:[OLImage imageWithContentsOfFile:filePath] ];
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(imageReady:) name:@"imageReady" object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(initialStart:) name:@"initialImageAvailable" object:nil];
@@ -51,6 +49,7 @@
     UILongPressGestureRecognizer *copytouch = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPress:)];
     [copytouch setMinimumPressDuration:1.0];
     [[self view] addGestureRecognizer:copytouch];
+    
     [[self view] addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(quickTap:)]];
 }
 
@@ -98,12 +97,10 @@
         [self.progress setHidden:NO];
         NSURL *url = [NSURL URLWithString:self.imageURL];
         NSURLRequest *request = [NSURLRequest requestWithURL:url];
-        //NSLog(@"Grabbing %@", self.imageURL);
         AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
         [operation setDownloadProgressBlock:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
             float sofar = ((float)((int)totalBytesWritten) / (float)((int)totalBytesExpectedToWrite));
             if(sofar - [self.progress progress] > 0.03) {
-                //NSLog(@"%lld/%lld = %f", totalBytesWritten, totalBytesExpectedToWrite, sofar);
                 [self.progress setProgress:sofar animated:YES];
             }
             
@@ -111,13 +108,9 @@
         [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *request, id data) {
             imageData = data;
             [[NSNotificationCenter defaultCenter] postNotificationName:@"imageReady" object:imageView];
-            [TestFlight passCheckpoint:@"gif_loaded"];
-            [self.view setBackgroundColor:[UIColor blackColor]];
         } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-            NSLog(@"%@", error);
             [self.loadingText setText:@"THIS KITTEN BROKE D-:"];
-            [self.progress setProgressTintColor:[UIColor redColor]];
-            [self.progress setProgress:1];
+            [self.progress setHidden:YES];
             TFLog(@"GIFAIL on %@: %@", self.imageURL, error);
         }];
         [operation start];
@@ -137,6 +130,8 @@
 - (void)imageReady:(NSNotification *)sender
 {
     if([sender object] == imageView) {
+        [TestFlight passCheckpoint:@"gif_loaded"];
+        [self.view setBackgroundColor:[UIColor blackColor]];
         [self.progress setHidden:YES];
         [self.progress setProgress:0 animated:NO];
         [loadingText setHidden:YES];
